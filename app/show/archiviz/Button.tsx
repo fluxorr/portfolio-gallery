874a0dc684b9f805704b6800656ee02edcdf9529
Rahul Chaudhari_ <rahulgc545@gmail.com>
2026-03-13 20:00:49 +0530

    init

---
+++ app/show/archiviz/Button.tsx
@@ -0,0 +1,27 @@
+import { cn } from "@/lib/utils"
+import { cva } from "class-variance-authority"
+
+
+const buttonVariants = cva{
+    "", {
+        variants: {
+            default: "",
+
+        }
+    }
+}
+
+
+const Button = () => {
+
+
+
+
+    return (
+        <div>
+            i am button
+        </div>
+    )
+}
+
+export default Button
